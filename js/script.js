00462da97476abe81708ab88a6ece57e686ea0b9
Diana Garcia <dianasgr12@gmail.com>
2026-08-21 17:50:22 -0600

    feat(index.html,script.js): se lanza primera versión de la página y variables de nombre de peliculas

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/** Esta variable en lista los 10 video juegos que quiero recomedar
+ * La utulizo ya que en varias partes del html las usare
+ */
+let listVideoGame = [
+    , "1. Project zomboid"
+    , "2. Pokemon Unite"
+    , "3. Dont strave"
+    , "4. Overcooked"
+    , "5. Terraria"
+    , "6. Elsword"
+    , "7. GTA 5"
+    , "8. Heatopia"
+    , "9. Roblox"
+    , "10. Among us"
+];
+let mensaje = "Hola, puedes ver mi pagina web";
+let mensaje = "1. Project zomboid";
+
+let topUno = 1;
+let topdos = 2;
+
+let bandera = true;
+
