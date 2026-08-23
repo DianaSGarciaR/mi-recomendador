4ab28b90cc24a1598b1f5777d256c7160a573fbe
DianaSGarciaR <78896448+DianaSGarciaR@users.noreply.github.com>
2026-08-23 20:25:11 +0200

    docs(script.js) Se agregan y modifican los comentarios
    docs(script.js) Se modifica los comentarios
    
    Se agregan y modifican los comentarios de las variables, se específica que son y para que se utilizaran.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-/** Esta variable en lista los 10 video juegos que quiero recomedar
- * La utulizo ya que en varias partes del html las usare
- */
+/** Variable array que almacena una lista de los 10 video juegos que quiero recomendar */
 let listVideoGame = [
     , "1. Project zomboid"
     , "2. Pokemon Unite"
@@ -13,15 +11,21 @@ let listVideoGame = [
     , "9. Roblox"
     , "10. Among us"
 ];
-let mensaje = "Hola, puedes ver mi pagina web";
+
+/** Variable que almacena el mensaje de lo que quiero presentar */
+let mensaje = "Hola, puedes ver mi página web";
+/** Variable que almacena el nombre de mi video juego favorito el cual esta como el número uno de mis recomendaciones */
 let miVideoGameFavorito = "Project zomboid";
 
+/** Variable de tipo numérico que almacena el numero 1 el cual se usara para mostrar en un mensaje de consola */
 let numeroVideoGame = 1;
+/** Variable de tipo numérico que almacena el numero 10 el cual se usara para mostrar en un mensaje de consola */
 let numeroMiFavorito = 10;
 
-
+/** Variable de tipo booleana la cuan inicializa como TRUE, se usará para mostrar en un mensaje de consola */
 let bandera = true;
 
+/** Mensajes de consola donde se usan las variables declaras */
 console.log(mensaje);
 console.log("Vista activa: ", bandera)
 console.log("Mi video juego favorito: es el Top ", numeroVideoGame, "y su nombre es: ", miVideoGameFavorito);
